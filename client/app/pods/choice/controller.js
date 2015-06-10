081f32953a6e4d8fc929dab6d669f6ce17cd7db3
Lamar Meigs <lamar.meigs@sykes.com>
2015-06-10 13:52:44 -0500

    Completed implementing choices checkboxes

--- client/app/pods/choice/controller.js
+++ client/app/pods/choice/controller.js
@@ -13,20 +13,36 @@ export default Ember.Controller.extend({
     .then((transitions) =>
       transitions.objectAt(0).get('output'))
 
-    .then((gatewayDef) =>
-      gatewayDef.getOutgoingTransitions())
+    .then((gatewayDef) => {
+      var subclass= gatewayDef.get('subclass');
+      if (subclass !== 'ExclusiveGatewayDef'
+          && subclass !== 'InclusiveGatewayDef'
+          && subclass !== 'ParallelGatewayDef') {
+        return null;
+      }
+      return gatewayDef.getOutgoingTransitions()
 
-    .then((validTransitions) => {
-      return validTransitions.map(
-        (validTransition) => {
+      .then((validTransitions) =>
+        validTransitions.map((validTransition) => {
           return Ember.Object.create({
             isChecked: false,
             validTransition: validTransition
           });
         })
+      );
     });
 
     return DS.PromiseArray.create({promise:promise})
   }.property('model'),
+
+  selectedChoices: function() {
+    var selected = this.get('validChoices').filterBy('isChecked', true);
+    var choice = this.get('model');
+    var newChoices = "";
+    selected.forEach(function(item, index, enumerable) {
+      newChoices = newChoices + item.get('validTransition.name') + " ";
+    });
+    choice.set('choices', newChoices);
+  }.observes('validChoices.@each.isChecked'),
 
 });
