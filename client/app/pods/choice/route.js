0e98961329f5774114b3de53756ef4b6baba01b2
pscottdevos <scott.devos@sykes.com>
2015-06-10 14:22:54 -0500

    remove error checking on choice submit.
    (it breaks the new choice task)

--- client/app/pods/choice/route.js
+++ client/app/pods/choice/route.js
@@ -15,14 +15,7 @@ export default Ember.Route.extend(TaskMixin, {
 
   actions:{
     submit: function() {
-      var choices = this.get('controller.model').get('choices');
-      var validChoices = ['A', 'B', 'A B', 'B A', 'C', 'D', 'C D', 'D C'];
-      if (validChoices.indexOf(choices) === -1) {
-        Ember.$('#errorLabel').text('Invalid Choice');
-      } else {
-        Ember.$('#errorLabel').text('');
-        this.doSubmit();
-      }
+      this.doSubmit();
     }
   }
 });
