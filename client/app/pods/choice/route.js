7a386885f1d17b6576b3a78d3426bae3b2218507
pscottdevos <scott.devos@sykes.com>
2015-06-10 06:44:21 -0500

    Add some simple error checking for choice tasks.

--- client/app/pods/choice/route.js
+++ client/app/pods/choice/route.js
@@ -15,7 +15,14 @@ export default Ember.Route.extend(TaskMixin, {
 
   actions:{
     submit: function() {
-      this.doSubmit();
+      var choices = this.get('controller.model').get('choices');
+      var validChoices = ['A', 'B', 'A B', 'B A', 'C', 'D', 'C D', 'D C'];
+      if (validChoices.indexOf(choices) === -1) {
+        Ember.$('#errorLabel').text('Invalid Choice');
+      } else {
+        Ember.$('#errorLabel').text('');
+        this.doSubmit();
+      }
     }
   }
 });
