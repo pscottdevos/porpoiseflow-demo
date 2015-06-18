84ca23d5d0a6e90bc0c772ef6e022757645cfc5f
pscottdevos <scott.devos@sykes.com>
2015-06-18 08:45:07 -0500

    946 - be able to switch processes mid process
    Fixed process-link to not link directly to in progress subprocesses

--- client/app/pods/process-defs/route.js
+++ client/app/pods/process-defs/route.js
@@ -3,25 +3,5 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model: function(params) {
     return this.store.find('porpoiseflow/processDef');
-//    return [
-//      {
-//        enabled: true,
-//        date: '5/26/2015',
-//        name: 'Demo ProcessDef',
-//        process_id: 1,
-//        process_type: 'demo',
-//        description: 'A demo process def',
-//        superceded_by: null
-//      },
-//      {
-//        enabled: true,
-//        date: '5/24/2015',
-//        name: 'Demo ProcessDef 2',
-//        process_id: 2,
-//        process_type: 'demo',
-//        description: 'A second demo process def',
-//        superceded_by: null
-//      }
-//    ];
   }
 });
