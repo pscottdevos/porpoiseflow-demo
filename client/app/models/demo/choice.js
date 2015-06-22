81ea9b69cc5fb00b669cec3ad6888d0e3de60eb9
Lamar Meigs <lamar.meigs@sykes.com>
2015-06-22 15:14:58 -0500

    BGZ 978: Alter ChoiceController validChoices based on Choice model
    If the choice model has an associated NodeDefProperty in which
    multiple_choice = 'false' then display the text field. Otherwise, display the
    checkboxes

--- client/app/models/demo/choice.js
+++ client/app/models/demo/choice.js
@@ -2,5 +2,6 @@ import DS from 'ember-data';
 import Task from '../porpoiseflow/task';
 
 export default Task.extend({
-  choices: DS.attr('string')
+  choices: DS.attr('string'),
+  multiple_choice: DS.attr('string', { defaultValue: 'true' })
 });
--- client/app/pods/choice/controller.js
+++ client/app/pods/choice/controller.js
@@ -5,29 +5,51 @@ export default Ember.Controller.extend({
 
   validChoices: function() {
     var choice = this.get('model');
+    var nodeDef;
+
     var promise = choice.get('taskNode.nodeDef')
     
-    .then((nodeDef) =>
-      nodeDef.getOutgoingTransitions())
+    .then((fetchedNodeDef) => {
+      // first, figure out if we display checkboxes at all
+      nodeDef = fetchedNodeDef;
+      return this.store.find('porpoiseflow/node-def-property', {
+        node_def: nodeDef.get('id'),
+        name: 'multiple_choice'
+      })
 
-    .then((transitions) =>
-      transitions.objectAt(0).get('output'))
+      .then((nodeProperties) => {
+        if (nodeProperties.get('length') > 0) {
+          var nodeProperty = nodeProperties.objectAt(0);
+          if (nodeProperty.get('value') === 'false') {
+            return [];
+          }
+        }
 
-    .then((gatewayDef) => {
-      var subclass= gatewayDef.get('subclass');
-      if (
-        subclass !== 'ExclusiveGatewayDef' && subclass !== 'InclusiveGatewayDef' && subclass !== 'ParallelGatewayDef'
-      ) { return null; }
-      return gatewayDef.getOutgoingTransitions()
+        // multiple_choice == true  => find transitions to use as choices
+        return nodeDef.getOutgoingTransitions()
 
-      .then((validTransitions) =>
-        validTransitions.map((validTransition) => {
-          return Ember.Object.create({
-            isChecked: false,
-            validTransition: validTransition
-          });
-        })
-      );
+        .then((transitions) =>
+          transitions.objectAt(0).get('output'))
+
+        .then((gatewayDef) => {
+          var subclass= gatewayDef.get('subclass');
+          if (
+            subclass !== 'ExclusiveGatewayDef' &&
+            subclass !== 'InclusiveGatewayDef' &&
+            subclass !== 'ParallelGatewayDef'
+          ) { return null; }
+          return gatewayDef.getOutgoingTransitions()
+
+          .then((validTransitions) =>
+            validTransitions.map((validTransition) => {
+              return Ember.Object.create({
+                isChecked: false,
+                validTransition: validTransition
+              });
+            })
+          );
+        });
+      });
     });
 
     return DS.PromiseArray.create({promise:promise});
