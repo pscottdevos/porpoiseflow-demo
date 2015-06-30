672356b36df1c913962c038a4e7bebd6a62e1e28
David Jackson <david.jackson@sykes.com>
2015-06-30 11:52:54 -0500

    Added sorting for process-defs route and choice controller.

--- client/app/pods/process-defs/route.js
+++ client/app/pods/process-defs/route.js
@@ -2,6 +2,11 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function(params) {
-    return this.store.find('porpoiseflow/processDef');
+    return this.store.find('porpoiseflow/processDef')
+    .then((processDefs) => {
+      processDefs.set('sortProperties', ['name']);
+      processDefs.set('sortAscending', true);
+      return processDefs;
+    });
   }
 });
--- client/tests/unit/process-defs/route-test.js
+++ client/tests/unit/process-defs/route-test.js
@@ -1,11 +1,19 @@
 import { moduleFor, test } from 'ember-qunit';
+import fakeStore from 'client/tests/helpers/fake-store';
 
 moduleFor('route:process-defs', 'Unit | Route | process defs', {
   // Specify the other units that are required for this test.
   // needs: ['controller:foo']
 });
 
-test('it exists', function(assert) {
+test('it sorts process defs by name', function(assert) {
   var route = this.subject();
-  assert.ok(route);
+
+  route.store = fakeStore().alwaysFinds([]);
+  
+  route.model({})
+  .then((processDefs) => {
+    assert.deepEqual(processDefs.get('sortProperties'), ['name']);
+    assert.strictEqual(processDefs.get('sortAscending'), true);
+  });
 });
